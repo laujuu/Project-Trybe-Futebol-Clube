87449101025ee701d6510846dcb6a3fb766d46b1
laujuu <lauroindcontato@gmail.com>
2022-11-22 18:05:34 -0300

    :sparkles: feat: Adiciona User Model e Migration

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,40 @@
+import { Model, INTEGER, STRING, DataTypes } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  // <campo>: <tipo>;
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  // ... Campos
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+  },
+  role: {
+    type: STRING,
+  },
+  email: {
+    type: STRING,
+  },
+  password: {
+    type: STRING,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'Users',
+  timestamps: false,
+});
+
+export default User;
